ba90908d430c49986a0d5a72ad5e6e8e3b60e4b3
Valery Piashchynski <piashchynski.valery@gmail.com>
2022-05-25 08:54:54 +0200

    simple_middleware
    Signed-off-by: Valery Piashchynski <piashchynski.valery@gmail.com>

---
+++ middleware/simple_middleware/middleware.go
@@ -0,0 +1,25 @@
+package simple_middleware
+
+import (
+	"net/http"
+
+	"go.uber.org/zap"
+)
+
+type Plugin struct {
+	log *zap.Logger
+}
+
+func (p *Plugin) Init(log *zap.Logger) error {
+	p.log = &zap.Logger{}
+	*p.log = *log
+	return nil
+}
+
+// Middleware is a special interface which after registration, RR will find automatically and add it to the http chain
+func (p *Plugin) Middleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		p.log.Info("i'm here")
+		next.ServeHTTP(w, r)
+	})
+}
