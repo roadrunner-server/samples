84d93ee412b4ef1247ba74ee2f2fa2245681aedd
Valery Piashchynski <piashchynski.valery@gmail.com>
2024-07-25 12:55:33 +0200

    chore: message.go
    Signed-off-by: Valery Piashchynski <piashchynski.valery@gmail.com>

--- plugins/jobs_driver/driver/driver.go
+++ plugins/jobs_driver/driver/driver.go
@@ -17,37 +17,46 @@ type Configurer interface {
 }
 
 type Driver struct {
+	queue jobs.Queue
 }
 
-func FromConfig(configKey string, log *zap.Logger, cfg Configurer, pipeline jobs.Pipeline, pq jobs.Queue) (*Driver, error) {
-	return &Driver{}, nil
+func FromConfig(configKey string, log *zap.Logger, cfg Configurer, pipeline jobs.Pipeline, pq jobs.Queue) (*Driver, error) { //nolint:revive
+	return &Driver{
+		queue: pq,
+	}, nil
 }
 
 // FromPipeline initializes consumer from pipeline
-func FromPipeline(pipeline jobs.Pipeline, log *zap.Logger, cfg Configurer, pq jobs.Queue) (*Driver, error) {
-	return &Driver{}, nil
+func FromPipeline(pipeline jobs.Pipeline, log *zap.Logger, cfg Configurer, pq jobs.Queue) (*Driver, error) { //nolint:revive
+	return &Driver{
+		queue: pq,
+	}, nil
 }
 
-func (d *Driver) Push(ctx context.Context, job jobs.Message) error {
+func (d *Driver) Push(_ context.Context, job jobs.Message) error {
+	item := fromJob(job)
+	d.queue.Insert(item)
 	return nil
 }
 
-func (d *Driver) Run(ctx context.Context, p jobs.Pipeline) error {
+func (d *Driver) Run(_ context.Context, _ jobs.Pipeline) error {
 	return nil
 }
 
-func (d *Driver) State(ctx context.Context) (*jobs.State, error) {
+func (d *Driver) State(_ context.Context) (*jobs.State, error) {
 	return nil, nil
 }
 
-func (d *Driver) Pause(ctx context.Context, p string) error {
+// Pause ctx + pipeline name
+func (d *Driver) Pause(_ context.Context, _ string) error {
 	return nil
 }
 
-func (d *Driver) Resume(ctx context.Context, p string) error {
+// Resume ctx + pipeline name
+func (d *Driver) Resume(_ context.Context, _ string) error {
 	return nil
 }
 
-func (d *Driver) Stop(ctx context.Context) error {
+func (d *Driver) Stop(_ context.Context) error {
 	return nil
 }
---
+++ plugins/jobs_driver/driver/message.go
@@ -0,0 +1,132 @@
+package driver
+
+import (
+	"encoding/json"
+	"time"
+
+	"github.com/roadrunner-server/api/v4/plugins/v4/jobs"
+)
+
+var _ jobs.Job = (*Item)(nil)
+
+const (
+	auto string = "deduced_by_rr"
+)
+
+type Item struct {
+	// Job contains the pluginName of job broker (usually PHP class).
+	Job string `json:"job"`
+	// Ident is a unique identifier of the job, should be provided from outside
+	Ident string `json:"id"`
+	// Payload is string data (usually JSON) passed to Job broker.
+	Payload []byte `json:"payload"`
+	// Headers with key-values pairs
+	headers map[string][]string
+	// Options contain a set of PipelineOptions specific to job execution. Can be empty.
+	Options *Options `json:"options,omitempty"`
+}
+
+// Options carry information about how to handle a given job.
+type Options struct {
+	// Priority is job priority, default - 10
+	// pointer to distinguish 0 as a priority and nil as a priority not set
+	Priority int64 `json:"priority"`
+	// Pipeline manually specified pipeline.
+	Pipeline string `json:"pipeline,omitempty"`
+	// Delay defines time duration to delay execution for. Defaults to none.
+	Delay int `json:"delay,omitempty"`
+	// AutoAck option
+	AutoAck bool `json:"auto_ack"`
+	// AMQP Queue
+	Queue string `json:"queue,omitempty"`
+}
+
+// DelayDuration returns delay duration in the form of time.Duration.
+func (o *Options) DelayDuration() time.Duration {
+	return time.Second * time.Duration(o.Delay)
+}
+
+func (i *Item) ID() string {
+	return i.Ident
+}
+
+func (i *Item) GroupID() string {
+	return i.Options.Pipeline
+}
+
+func (i *Item) Priority() int64 {
+	return i.Options.Priority
+}
+
+// Body packs job payload into binary payload.
+func (i *Item) Body() []byte {
+	return i.Payload
+}
+
+func (i *Item) Headers() map[string][]string {
+	return i.headers
+}
+
+// Context packs job context (job, id) into binary payload.
+// Not used in the amqp, amqp.Table used instead
+func (i *Item) Context() ([]byte, error) {
+	ctx, err := json.Marshal(
+		struct {
+			ID       string              `json:"id"`
+			Job      string              `json:"job"`
+			Driver   string              `json:"driver"`
+			Queue    string              `json:"queue"`
+			Headers  map[string][]string `json:"headers"`
+			Pipeline string              `json:"pipeline"`
+		}{
+			ID:       i.Ident,
+			Job:      i.Job,
+			Driver:   "my_awesome_driver",
+			Headers:  i.headers,
+			Queue:    i.Options.Queue,
+			Pipeline: i.Options.Pipeline,
+		},
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return ctx, nil
+}
+
+func (i *Item) Ack() error {
+	return nil
+}
+
+func (i *Item) NackWithOptions(requeue bool, delay int) error { //nolint:revive
+	return nil
+}
+
+func (i *Item) Nack() error {
+	return nil
+}
+
+// Requeue with the provided delay, handled by the Nack
+func (i *Item) Requeue(headers map[string][]string, delay int) error { //nolint:revive
+	return nil
+}
+
+func (i *Item) Respond(_ []byte, _ string) error {
+	return nil
+}
+
+func fromJob(job jobs.Message) *Item {
+	return &Item{
+		Job:     job.Name(),
+		Ident:   job.ID(),
+		Payload: job.Payload(),
+		headers: job.Headers(),
+		Options: &Options{
+			Priority: job.Priority(),
+			Pipeline: job.GroupID(),
+			Delay:    int(job.Delay()),
+			AutoAck:  job.AutoAck(),
+		},
+	}
+}
